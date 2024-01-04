edc1493b4f65878f35e41681b0654430fa6f3fad
RainBowT <rainbowt0506@gmail.com>
2024-01-04 18:43:37 +0800

    feat: Import and plot ESRI Shapefile of the area of interest using GeoPandas

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ districts = gpd.read_file(r'.\Shapefiles\districts.shp')
 districts.plot(cmap = 'hsv', edgecolor = 'black', column = 'district')
 plt.title('Districts Plot')
 plt.show()
+
+area_of_interest = gpd.read_file(r'.\Shapefiles\area_of_interest.shp')
+area_of_interest.plot()
+plt.title('Area of Interest Plot')
+plt.show()
