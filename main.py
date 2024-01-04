3d40b04e2804a4b3f141f3bf29fbfb3a6df4f6c1
RainBowT <rainbowt0506@gmail.com>
2024-01-04 18:46:31 +0800

    feat: Import and plot ESRI Shapefiles of districts, area of interest, and ATMs using GeoPandas

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ area_of_interest = gpd.read_file(r'.\Shapefiles\area_of_interest.shp')
 area_of_interest.plot()
 plt.title('Area of Interest Plot')
 plt.show()
+
+atms = gpd.read_file(r'.\Shapefiles\atms.shp')
+# Plot the figures side by side
+fig, (ax1, ax2) = plt.subplots(nrows = 2, figsize = (10,8))
+districts.plot(ax = ax1, cmap = 'hsv', edgecolor = 'black', column = 'district')
+area_of_interest.plot(ax = ax2, color = 'green')
+plt.suptitle('Districts and Area of Interest Plot')
+plt.show()
